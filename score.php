b7c25c711f1d9baf992ea48d105f2c9b2bd23183
Sijin <sijinchuzhali@gmail.com>
2015-02-21 01:53:30 +0530

    Hi
    PHP introduced

--- score.php
+++ score.php
@@ -4,21 +4,45 @@
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "scoreboard";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT year,total FROM score ORDER BY total DESC";
+$result = $conn->query($sql);
+?>
 	<div id="div-align">
 		<table id="table-wrapper">
-			<tr>
+			<tr><?php if (1) { ?>
 				<td><img src="images/s8.png" class="year"></td><td><button class="myButton">score1<!-- dynamic --></button></td>
+			
+				<?php } ?>
 			</tr>
-			<tr>
+
+			<tr><?php if (1) { ?>
 				<td><img src="images/s6.png" class="year"></td><td><button class="myButton">score2<!-- dynamic --></button></td>
+			<?php } ?>
 			</tr>
-			<tr>
+			<tr><?php if (1) { ?>
 				<td><img src="images/s4.png" class="year"></td><td><button class="myButton">score3<!-- dynamic --></button></td>
+			<?php } ?>
 			</tr>
-			<tr>
+			<tr><?php if (1) { ?>
 				<td><img src="images/s1&s2.png" class="year"></td><td><button class="myButton">score4<!-- dynamic --></button></td>
+			<?php } ?>
 			</tr>
 		</table>
 	</div>
+<php?	$conn->close();
+?>
 </body>
 </html>
